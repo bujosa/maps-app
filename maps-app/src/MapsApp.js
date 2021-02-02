32b86bb83f8e91506b2a75e17c67e28ef3373a6a
bujosa <davidbujosa@gmail.com>
2021-02-01 23:02:22 -0400

    Added initial setup on maps app

---
+++ maps-app/src/MapsApp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MapsApp = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
---
+++ maps-app/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { MapsApp } from "./MapsApp";
+
+ReactDOM.render(<MapsApp />, document.getElementById("root"));
