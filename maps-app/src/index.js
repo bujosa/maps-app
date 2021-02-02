f853294d9b82192682bf7dfdb74673927a1ad28f
bujosa <davidbujosa@gmail.com>
2021-02-02 00:03:27 -0400

    Added map app on index js

--- maps-app/src/index.js
+++ maps-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import { MapsApp } from "./MapsApp";
 
 ReactDOM.render(<MapsApp />, document.getElementById("root"));
