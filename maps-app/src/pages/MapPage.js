c46b848c221590b368f3e84f532f37479fea2d59
bujosa <davidbujosa@gmail.com>
2021-02-03 00:12:32 -0400

    Fixed initial Point

--- maps-app/src/pages/MapPage.js
+++ maps-app/src/pages/MapPage.js
@@ -5,9 +5,9 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiYnVqb3NhIiwiYSI6ImNra25ldzBlZDEzdnYydnBkZXNob2Q1a3cifQ.VQsp-0l_cbGfhyGnLnJigA";
 
 const initialPoint = {
-  lng: 5,
-  lat: 34,
-  zoom: 10,
+  lng: -69.8417,
+  lat: 18.5195,
+  zoom: 17.01,
 };
 
 export const MapPage = () => {
@@ -25,6 +25,18 @@ export const MapPage = () => {
     });
     setMap(mapbox);
   }, []);
+
+  useEffect(() => {
+    map?.on("move", () => {
+      const { lng, lat } = map.getCenter();
+      setCoords({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+    return map?.off("move");
+  }, [map]);
 
   return (
     <>
