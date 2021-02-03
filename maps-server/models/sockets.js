3d0f3dd3d74e2898f0b5d03c63dfa525bdbe93c7
bujosa <davidbujosa@gmail.com>
2021-02-03 01:38:20 -0400

    Added socket context

--- maps-server/models/sockets.js
+++ maps-server/models/sockets.js
@@ -1,13 +1,17 @@
+const Markers = require("./markers");
+
 class Sockets {
   constructor(io) {
     this.io = io;
-
+    this.markers = new Markers();
     this.socketEvents();
   }
 
   socketEvents() {
     // On connection
-    this.io.on("connection", (socket) => {});
+    this.io.on("connection", (socket) => {
+      this.socket.on("marker-online");
+    });
   }
 }
 
