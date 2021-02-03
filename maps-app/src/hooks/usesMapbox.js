1d45d4b74ecb2dbe86f9f0249075e978f5185720
bujosa <davidbujosa@gmail.com>
2021-02-03 00:31:53 -0400

    Fixed map page

--- maps-app/src/hooks/usesMapbox.js
+++ maps-app/src/hooks/usesMapbox.js
@@ -1,3 +1,49 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYnVqb3NhIiwiYSI6ImNra25ldzBlZDEzdnYydnBkZXNob2Q1a3cifQ.VQsp-0l_cbGfhyGnLnJigA";
+
+const initialPoint = {
+  lng: -69.8417,
+  lat: 18.5195,
+  zoom: 17.01,
+};
+
 export const usesMapbox = () => {
-  return {};
+  const mapDiv = useRef();
+
+  const setRef = useCallback((node) => {
+    mapDiv.current = node;
+  }, []);
+
+  const map = useRef();
+  const [coords, setCoords] = useState(initialPoint);
+
+  useEffect(() => {
+    const mapbox = new mapboxgl.Map({
+      container: mapDiv.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [initialPoint.lng, initialPoint.lat],
+      zoom: initialPoint.zoom,
+    });
+    map.current = mapbox;
+  }, []);
+
+  useEffect(() => {
+    map.current?.on("move", () => {
+      const { lng, lat } = map.current.getCenter();
+      setCoords({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.current.getZoom().toFixed(2),
+      });
+    });
+    return map.current?.off("move");
+  }, []);
+
+  return {
+    coords,
+    setRef,
+  };
 };
--- maps-app/src/pages/MapPage.js
+++ maps-app/src/pages/MapPage.js
@@ -1,48 +1,15 @@
-import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
+import React from "react";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiYnVqb3NhIiwiYSI6ImNra25ldzBlZDEzdnYydnBkZXNob2Q1a3cifQ.VQsp-0l_cbGfhyGnLnJigA";
-
-const initialPoint = {
-  lng: -69.8417,
-  lat: 18.5195,
-  zoom: 17.01,
-};
+import { usesMapbox } from "../hooks/usesMapbox";
 
 export const MapPage = () => {
-  const mapDiv = useRef();
-  const map = useRef();
-  const [coords, setCoords] = useState(initialPoint);
-
-  useEffect(() => {
-    const mapbox = new mapboxgl.Map({
-      container: mapDiv.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [initialPoint.lng, initialPoint.lat],
-      zoom: initialPoint.zoom,
-    });
-    map.current = mapbox;
-  }, []);
-
-  useEffect(() => {
-    map.current?.on("move", () => {
-      const { lng, lat } = map.current.getCenter();
-      setCoords({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.current.getZoom().toFixed(2),
-      });
-    });
-    return map.current?.off("move");
-  }, []);
-
+  const { coords, setRef } = usesMapbox();
   return (
     <>
       <div className="info">
         Lng: {coords.lng} | lat: {coords.lat} | zoom: {coords.zoom}
       </div>
-      <div ref={mapDiv} className="mapContainer" />
+      <div ref={setRef} className="mapContainer" />
     </>
   );
 };
