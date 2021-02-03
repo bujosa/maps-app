58bccd167db6c2cf24d81a72ec00209008359c73
bujosa <davidbujosa@gmail.com>
2021-02-02 23:57:28 -0400

    Added maps properties

--- maps-app/src/pages/MapPage.js
+++ maps-app/src/pages/MapPage.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYnVqb3NhIiwiYSI6ImNra25ldzBlZDEzdnYydnBkZXNob2Q1a3cifQ.VQsp-0l_cbGfhyGnLnJigA";
+
+const initialPoint = {
+  lng: 5,
+  lat: 34,
+  zoom: 10,
+};
 
 export const MapPage = () => {
-  return <div>Hola Mundo</div>;
+  const mapDiv = useRef();
+
+  const [, setMap] = useState();
+
+  useEffect(() => {
+    const mapbox = new mapboxgl.Map({
+      container: mapDiv.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [initialPoint.lng, initialPoint.lat],
+      zoom: initialPoint.zoom,
+    });
+    setMap(mapbox);
+  }, []);
+
+  return (
+    <>
+      <div ref={mapDiv} className="mapContainer" />
+    </>
+  );
 };
